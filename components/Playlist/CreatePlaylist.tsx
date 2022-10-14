1966301de4e9f185ed82495b3af18571379f3a76
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-10-14 19:12:27 +0200

    added modal

--- components/Playlist/CreatePlaylist.tsx
+++ components/Playlist/CreatePlaylist.tsx
@@ -1,12 +1,39 @@
 import React, { useState } from "react";
 import MusicNoteIcon from "@mui/icons-material/MusicNote";
 import EditIcon from "@mui/icons-material/Edit";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 import styles from "./styles.module.css";
+import { TextareaAutosize } from "@mui/base";
+import { style } from "@mui/system";
 
 function CreatePlaylist() {
   const [hover, setHover] = useState(false);
+  const [modalHover, setModalHover] = useState(false);
   const [image, setImage] = useState<File | null>(null);
   const [playlistName, setPlaylistName] = useState("My playlist");
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSaveChanges = (playlistName: string) => {
+    setOpen(false);
+    setPlaylistName(playlistName);
+  };
+
+  console.log(playlistName);
 
   return (
     <>
@@ -24,27 +51,95 @@ function CreatePlaylist() {
           ) : (
             <MusicNoteIcon />
           )}
-          <label
+          <span
             onMouseOver={() => setHover(true)}
             onMouseLeave={() => setHover(false)}
-            htmlFor="image"
             className={styles.input_label}
+            onClick={handleClickOpen}
           >
             {hover ? <EditIcon className={styles.edit_icon} /> : null}
-          </label>
-          <input
-            id="image"
-            className={styles.input}
-            type="file"
-            accept="image/png, image/jpeg"
-            onChange={(e) => setImage(e.target.files[0])}
-          />
+          </span>
         </div>
         <div className={styles.playlist_info}>
-          <span>{playlistName}</span>
-          <span>Username</span>
+          <span className={styles.playlist_name}>{playlistName}</span>
+          <span>julito23</span>
         </div>
       </div>
+      <Dialog open={open} onClose={handleClose}>
+        <div className={styles.modal}>
+          <DialogTitle
+            sx={{
+              color: "white",
+            }}
+          >
+            Edit Details
+          </DialogTitle>
+          <DialogContent
+            sx={{
+              display: "flex",
+            }}
+          >
+            <div
+              className={styles.modal_image_container}
+              onMouseOver={() => setModalHover(true)}
+              onMouseLeave={() => setModalHover(false)}
+            >
+              {image !== null && image !== undefined ? (
+                <img
+                  className={styles.modal_image_container}
+                  src={URL.createObjectURL(new Blob([image]))}
+                />
+              ) : (
+                <>
+                  <EditIcon className={styles.edit_icon} />
+                  <span>Select an image</span>
+                </>
+              )}
+              <label htmlFor="image" className={styles.input_label}>
+                {modalHover && image !== null && image !== undefined ? (
+                  <EditIcon className={styles.edit_icon} />
+                ) : null}
+              </label>
+              <input
+                id="image"
+                className={styles.input}
+                type="file"
+                accept="image/png, image/jpeg"
+                onChange={(e) => setImage(e.target.files[0])}
+              />
+            </div>
+            <div className={styles.modal_data}>
+              <TextField
+                sx={{
+                  color: "white",
+                }}
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Playlist name"
+                type="text"
+                fullWidth
+                variant="standard"
+              />
+              <TextareaAutosize
+                aria-label="minimum height"
+                minRows={3}
+                placeholder="Add an optional description"
+                style={{
+                  width: 200,
+                  backgroundColor: "#585c63",
+                }}
+              />
+            </div>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={() => handleSaveChanges(playlistName)}>
+              Save Changes
+            </Button>
+          </DialogActions>
+        </div>
+      </Dialog>
     </>
   );
 }
