644f5d65f8f51e4a239b01b06811dc99933c1104
Alejandro <alejandroaperez1994g@gmail.com>
2022-10-17 22:18:59 +0200

    feat: :sparkles: added banner component for liked songs page

---
+++ components/Banner/Banner.tsx
@@ -0,0 +1,37 @@
+import styles from './styles.module.css';
+import { Button } from '@mui/material';
+import Tooltip from '@mui/material/Tooltip';
+import Image from 'next/image';
+import { useI18N } from '../../context/i18';
+
+type Props = {
+  user: string;
+  total: number;
+};
+
+const Banner = ({ user, total }: Props) => {
+  const { t } = useI18N();
+  return (
+    <div className={styles.banner_container}>
+      <div className={styles.banner_front}>
+        <Image
+          className={styles.banner_image}
+          src="/Images/likeTracks.png"
+          alt={'bombay'}
+          width={200}
+          height={200}
+          layout="fixed"
+        />
+        <div className={styles.banner_content}>
+          <h1 className={styles.banner_text}>{t('headers').headerFavorites}</h1>
+          <h4 className={styles.banner_user_details}>
+            {/* <InterpreterModeIcon /> */}
+            {user} - {total} {t('additional').tracks}
+          </h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
