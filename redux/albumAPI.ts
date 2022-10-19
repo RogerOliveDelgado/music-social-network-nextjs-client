faaae226c7730929568704493ad753266d16d621
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-10-19 10:19:12 +0200

    fixed merge conflict

--- redux/albumAPI.ts
+++ redux/albumAPI.ts
@@ -1,15 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const API =
-<<<<<<< HEAD
   process.env.NEXT_PUBLIC_BACKEND_SPOTIFY_BACKEND || "http://localhost:4002";
 
-=======
-  process.env.NEXT_PUBLIC_BACKEND_SPOTIFY_BACKEND ||
-  // 'http://ec2-44-211-78-241.compute-1.amazonaws.com:4000/';
-  "http://localhost:4002";
-  
->>>>>>> 11d1a22e4732792e28d13b103847ba3104ca2947
 export const albumAPI = createApi({
   reducerPath: "albumAPI",
   baseQuery: fetchBaseQuery({ baseUrl: API }),
