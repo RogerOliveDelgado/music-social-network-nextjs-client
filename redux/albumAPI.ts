11d1a22e4732792e28d13b103847ba3104ca2947
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-10-19 10:13:26 +0200

    modified route to api

--- redux/albumAPI.ts
+++ redux/albumAPI.ts
@@ -2,8 +2,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const API =
   process.env.NEXT_PUBLIC_BACKEND_SPOTIFY_BACKEND ||
-  'http://ec2-44-211-78-241.compute-1.amazonaws.com:4000/';
-
+  // 'http://ec2-44-211-78-241.compute-1.amazonaws.com:4000/';
+  "http://localhost:4002";
+  
 export const albumAPI = createApi({
   reducerPath: 'albumAPI',
   baseQuery: fetchBaseQuery({ baseUrl: API }),
--- redux/artistAPI.ts
+++ redux/artistAPI.ts
@@ -1,15 +1,16 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const API =
   process.env.NEXT_PUBLIC_BACKEND_SPOTIFY_BACKEND ||
-  'http://ec2-44-211-78-241.compute-1.amazonaws.com:4000/';
+  // 'http://ec2-44-211-78-241.compute-1.amazonaws.com:4000/';
+  "http://localhost:4002";
 
 export const artistAPI = createApi({
-  reducerPath: 'artistAPI',
+  reducerPath: "artistAPI",
   baseQuery: fetchBaseQuery({ baseUrl: API }),
   endpoints: (builder) => ({
     getArtists: builder.query({
-      query: () => '/artist',
+      query: () => "/artist",
     }),
     getArtistDetails: builder.query({
       query: (artistID) => `/artist/${artistID}`,
