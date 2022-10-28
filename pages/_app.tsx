7234077bb89b6bf82093fc1815e5b8fe613444cf
Alejandro <alejandroaperez1994g@gmail.com>
2022-10-28 17:31:33 +0200

    feat: :sparkles: added progress bar between pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,26 @@
-import type { AppProps } from "next/app";
-import Player from "../components/Player/Player";
-import { Provider } from "react-redux";
-import { store } from "../redux/store";
-import { I18NProvider } from "../context/i18";
-import { CookiesProvider } from "react-cookie";
-import "../styles/globals.css";
+import type { AppProps } from 'next/app';
+import Player from '../components/Player/Player';
+import { Provider } from 'react-redux';
+import { store } from '../redux/store';
+import { I18NProvider } from '../context/i18';
+import { CookiesProvider } from 'react-cookie';
+import Router from 'next/router';
+import ProgressBar from '@badrap/bar-of-progress';
+import '../styles/globals.css';
+
+const progress = new ProgressBar({
+  size: 6,
+  color: '#695ee4',
+  className: 'z-50',
+  delay: 100,
+});
+
+Router.events.on('routeChangeStart', progress.start);
+Router.events.on('routeChangeComplete', progress.finish);
+Router.events.on('routeChangeError', progress.finish);
 
 type ComponentWithPageLayout = AppProps & {
-  Component: AppProps["Component"] & {
+  Component: AppProps['Component'] & {
     PageLayout?: React.ComponentType;
   };
 };
