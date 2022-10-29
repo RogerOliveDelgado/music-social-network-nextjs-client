11c88002018dbad43c48e617f420db184abd1ad4
Alejandro <alejandroaperez1994g@gmail.com>
2022-10-29 18:49:08 +0200

    feat: :sparkles: added animation on page load and tracklist

--- components/Row/Row.tsx
+++ components/Row/Row.tsx
@@ -1,9 +1,10 @@
-import AlbumCard from "../AlbumCard/AlbumCard";
-import { Artist } from "../../interfaces/ServerResponse";
-import { Album } from "../../interfaces/ServerResponse";
-import { Playlist } from "../../interfaces/playlistResponse";
+import AlbumCard from '../AlbumCard/AlbumCard';
+import { Artist } from '../../interfaces/ServerResponse';
+import { Album } from '../../interfaces/ServerResponse';
+import { Playlist } from '../../interfaces/playlistResponse';
+import { motion } from 'framer-motion';
 
-import styles from "./styles.module.css";
+import styles from './styles.module.css';
 
 type Props = {};
 
@@ -16,9 +17,20 @@ const Row = ({ title, data, children }: any) => {
       <div className={styles.horizontal_scroll}>
         {!children &&
           data &&
-          data.slice(0, 11).map((item: Album | Artist | Playlist) => {
-            return <AlbumCard key={item._id} item={item} />;
-          })}
+          data
+            .slice(0, 11)
+            .map((item: Album | Artist | Playlist, index: number) => {
+              return (
+                <motion.div
+                  key={item._id}
+                  initial={{ opacity: 0, x: 100 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  transition={{ delay: 0.01 + index * 0.1 }}
+                >
+                  <AlbumCard key={item._id} item={item} />
+                </motion.div>
+              );
+            })}
         {children}
       </div>
     </div>
